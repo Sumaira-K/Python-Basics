083a4d2958b36db704ad09bc4ad23e114945dab0
Sumaira-K <sumairakhan.reveuse@gmail.com>
2026-07-14 23:21:40 +0530

    Updated calculator and added new test files

---
+++ Test_Calculator_3.py
@@ -0,0 +1,16 @@
+#using pytest
+
+from Calculator import square
+import pytest
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+def test_zero():
+    assert square(0) == 0
+def test_str():
+    with pytest.rasies(TypeError):
+        square("cat")
