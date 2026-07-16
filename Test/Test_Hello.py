89e3c4f830ff2636f628348c120d61a18c0f0dea
Sumaira-K <sumairakhan.reveuse@gmail.com>
2026-07-16 17:14:32 +0530

    Remove .gitignore and Test folder

--- Test/Test_Hello.py
+++
@@ -1,7 +0,0 @@
-from Hello import hello
-def test_default():
-    #hello("Sumaira") == "Hello, Sumaira"
-    assert hello() == "Hello, World!"
-
-def test_arguement():
-    assert hello("Sumaira") == "Hello, Sumaira"
